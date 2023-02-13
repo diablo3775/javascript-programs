f4a4678b5bebd0fae913c45b91cada89453be1fa
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-02-13 21:33:59 +0530

    Update 3.nThHighest.js

--- arrays/3.nThHighest.js
+++ arrays/3.nThHighest.js
@@ -1,12 +1,7 @@
 //finding the nth highest element in the array;
-function maxi(arr,n) {
-    let highest = arr[0];
-    for(let i = 0;i < arr.length;i++) {
-      if(arr[i] > highest) {
-        highest = arr[i];
-      }
-    }
-    return highest - n;
-  }
-  
-  console.log(maxi([1,2,3,4,5,6,7,8],1))
+function nthHighest(arr, n) {
+    arr.sort((a, b) => b - a);
+    return arr[n - 1];
+}
+
+console.log(nthHighest([1, 2, 45, 56, 67], 1));
