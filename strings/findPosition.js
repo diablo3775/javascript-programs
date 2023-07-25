941ca7147a4834d9013482ee225bb5a1b80cd1b9
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-07-25 12:12:08 +0530

    Create findPosition.js

---
+++ strings/findPosition.js
@@ -0,0 +1,17 @@
+//Determine the position of the word "Nemo" and return a string in the following manner: "I found Nemo at [position of the word nemo]!"
+
+//If there are multiple instances of the word, return the string for the first occurence.
+//If you can't find Nemo, return "I can't find Nemo :("
+
+const str = "I am finding Nemo";
+
+const findStringPosition = (str, check) => {
+  let result = str.split(' ')
+  for(let i = 0;i < result.length;i++) {
+    if(result[i] === check) {
+      return `I found ${check} at ${i + 1}`
+    }
+  }  
+    return `I can't find ${check}!`;
+}
+console.log(findStringPosition(str, "Nemo"));
