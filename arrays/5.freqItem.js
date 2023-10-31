4ce4d08038f85f8bbb8dd581d5f2fe52dde3ff12
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-10-31 15:08:09 +0530

    Update 5.freqItem.js

--- arrays/5.freqItem.js
+++ arrays/5.freqItem.js
@@ -1,23 +1,22 @@
 //frequent item in an array
 let arr = [1,2,3,4,4,7,7,7,7,7,5]
 
-let b = {};
+let obj = {};
 for(let i = 0; i < arr.length; i++) {
-  b[arr[i]] = b[arr[i]] + 1 || 1;
+  obj[arr[i]] = obj[arr[i]] + 1 || 1;
 }
 
 let highestValue = arr[0];
-for(const pro in b) {
-  if(b[pro] > highestValue) {
-    highestValue = b[pro]
+for(const keys in obj) {
+  if(obj[keys] > highestValue) {
+    highestValue = obj[keys]
   }
 }
 
 let highestKey = arr[0];
-for(const pro in b) {
-  if(b[pro] === highestValue) {
-   highestKey = pro;
+for(const values in obj) {
+  if(obj[values] === highestValue) {
+   highestKey = values;
   }
 }
 console.log(highestKey)
-
